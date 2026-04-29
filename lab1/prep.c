d24b6d06991d0a9427bb9d2803bbb412b6b29663
Jacky161 <jacky.161@outlook.com>
2026-04-29 11:11:32 -0600

    l1prep done

--- lab1/prep.c
+++ lab1/prep.c
@@ -1,4 +1,5 @@
-// includes here...
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     int rows; // number of rows
@@ -8,25 +9,53 @@ typedef struct {
 
 // initialize matrix pointed to by m with r rows and c columns
 // i.e. allocate sufficient memory and set all elements to 0
-void init(Matrix* m, int rows, int cols);
+void init(Matrix* m, int rows, int cols)
+{
+    m->rows = rows;
+    m->cols = cols;
+    m->a = calloc((size_t)(rows * cols), sizeof(double));
+}
 
 // free memory associated with matrix pointed to by m
-void deallocate(Matrix* m);
+void deallocate(Matrix* m)
+{
+    free(m->a);
+}
 
 // set the value of the element at (row, col) to the input value
-void set(Matrix* m, int row, int col, double value);
+void set(Matrix* m, int row, int col, double value)
+{
+    m->a[m->cols * row + col] = value;
+}
 
 // returns a pointer-to-const to the value of the element at (row, col)
-const double* get(const Matrix* m, int row, int col);
+const double* get(const Matrix* m, int row, int col)
+{
+    return &m->a[m->cols * row + col];
+}
 
 // print the value of the element at (row, col) to console with 2 decimal places
-void printCell(const Matrix* m, int row, int col);
+void printCell(const Matrix* m, int row, int col)
+{
+    printf("%.2f", *get(m, row, col));
+}
 
 // print the entire matrix in the following format with 2 decimal places
 // i.e. 1.00 2.00 3.00
 //      4.00 5.00 6.00
 //      7.00 8.00 9.00
-void printMatrix(const Matrix* m);
+void printMatrix(const Matrix* m)
+{
+    for (int r = 0; r < m->rows; ++r)
+    {
+        for (int c = 0; c < m->cols; ++c)
+        {
+            printCell(m, r, c);
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
 
 int main(void)
 {
@@ -46,6 +75,7 @@ int main(void)
 
     // Test your Matrix here
     // i.e. printCell() and printMatrix()
+    printMatrix(&m);
 
     deallocate(&m);
     return 0;
